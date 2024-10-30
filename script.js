d1bbb6746e3d181188ab484f8437af5ae61786c7
norhanlidasan <norhanlidasanwpdev@gmail.com>
2024-10-30 06:44:14 +0300

    add clearDisplay and removeZero

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const numbers = document.querySelectorAll(".number");
 const currentDisplay = document.querySelector(".current-display");
-
+const del = document.querySelector(".delete");
 let display = 0;
+
 
 numbers.forEach(number => {
     currentDisplay.textContent = display;
     number.addEventListener('click', () => {
+            removeZero();
             display = null;     
             currentDisplay.textContent += number.textContent;
     })
 })
+
+function removeZero(){
+    if(currentDisplay.textContent == 0){
+        currentDisplay.textContent = ''
+    };
+}
+
+function clearDisplay(){
+    del.addEventListener('click', ()=>{
+        currentDisplay.textContent = 0;
+    })
+}
+clearDisplay();
