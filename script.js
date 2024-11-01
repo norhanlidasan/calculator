3a24926ece38a63294d9d3cb2cd46919d443c63f
norhanlidasan <177215771+norhanlidasan@users.noreply.github.com>
2024-11-01 14:55:00 +0300

    copy the code from rps-calculator

--- script.js
+++ script.js
@@ -1,9 +1,86 @@
 const numbers = document.querySelectorAll(".number");
-const firstOperand = document.querySelector(".first-operand");
+const currentDisplay = document.querySelector(".current-display");
+const previousDisplay = document.querySelector(".previous-display");
+const addition = document.querySelector(".add");
+const equal = document.querySelector("#equal")
+const del = document.querySelector(".delete");
 
-numbers.forEach( number => {
+
+function appendNumber(){
+numbers.forEach(number => {
     number.addEventListener('click', () => {
-        let firstNumber = document.createElement("span")
-        firstOperand.appendChild(firstNumber).textContent = number.value;
+            removeZero();   
+            currentDisplay.textContent += number.textContent;
     })
 })
+}
+
+appendNumber();
+function removeZero(){
+    if(currentDisplay.textContent == 0){
+        currentDisplay.textContent = '';
+    };
+}
+
+function clearDisplay(){
+    del.addEventListener('click', ()=>{
+        currentDisplay.textContent = 0;
+        previousDisplay.textContent = "";
+    })
+}
+clearDisplay();
+
+function addButton(){
+    addition.addEventListener('click', (e) => {
+
+        if (currentDisplay.textContent == 0 || currentDisplay.textContent == '') {
+            e.preventDefault();
+        } else {
+            previousDisplay.textContent = `${currentDisplay.textContent} +`;
+            currentDisplay.textContent = 0;
+        }
+
+    })
+}
+addButton();
+
+
+
+equal.addEventListener('click', evaluate);
+
+function evaluate() {;
+    let previous = previousDisplay.textContent;
+    let preDigit = previous.slice(0,-1);
+    let current = currentDisplay.textContent;
+    let operation = previous.at(-1);
+    let result;
+    console.log(preDigit);
+    
+    switch (operation){
+
+        case '+':
+        result = add(Number(preDigit),Number(current));
+        console.log(result);
+        currentDisplay.textContent = result;
+        break;
+    }
+    
+}
+
+
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
